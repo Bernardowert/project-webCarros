9e843779b30ebaf50ce6f48088f24f0f28f50549
devBernardo <bernardowerty@outlook.com>
2025-07-17 16:05:59 -0300

    criação da pagina cadastro de carros

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,9 +1,26 @@
+import { FiUpload } from "react-icons/fi";
+import { ContainerGRID } from "../../../components/containerGRID";
+import { DashboardHeader } from "../../../components/panelHeader";
+
 export function New() {
   
 
   return (
-    <div>
-      <h1>pagina cADASTRAR CARRO</h1>
-    </div>
+    <ContainerGRID>
+      <DashboardHeader/>
+
+      <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+          <button className="border-2 w-48 rounded-lg flex items-center justify-center border-gray-600 h-32">
+              <div className="absolute cursor-pointer">
+                <FiUpload size={30} color="#000"/>
+              </div>
+              <div className="cursor-pointer">
+                 <input type="file" accept="image/*" className="opacity-0 cursor-pointer"/>
+              </div>
+          </button>
+      </div>
+
+      
+    </ContainerGRID>
   )
 }
